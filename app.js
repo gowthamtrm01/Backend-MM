153f24165d0ce4e096fbb16acc0bb62768072f90
Gowtham Chandrasekar <gowthamtrm01@gamil.com>
2021-07-06 18:09:35 +0530

    update

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 5000;
 
 app.use(express.json());
 const mongoose = require('mongoose');
